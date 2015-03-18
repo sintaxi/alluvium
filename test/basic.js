2bf7b069a112725814cf81a6b6f9bb2bf9177c7f
Brock Whitten <brock@chloi.io>
2015-03-17 19:37:32 -0700

    adds mock input data for testing

--- test/basic.js
+++ test/basic.js
@@ -1,6 +1,17 @@
-var alluvium = require("../")
+var fs        = require("fs")
+var alluvium  = require("../").createClient()
+var ndjson    = require("ndjson")
+
 
 describe("alluvium", function(){
+
+  before(function(done){
+    process.stdout.write(" ")
+    fs.createReadStream(__dirname + '/input-data.ndjson')
+    .pipe(ndjson.parse())
+    .on('data', alluvium.log)
+    .on('end', done)
+  })
 
   it("should be cool", function(done){
     done()
