3a6794e619a7b4b22bc08258bfa7d313a5a2752c
Brock Whitten <brock@sintaxi.com>
2021-03-28 02:14:34 -0700

    fixes ua ref

--- test/samples/sintaxi.com/mocks.js
+++ test/samples/sintaxi.com/mocks.js
@@ -14,10 +14,10 @@ module.exports =
   , { ip: "456", timestamp: "2021-01-01", domain: "sintaxi.com", status: 304, method: "get", device: "desktop", path: "/contact", referrer: null }
   , { ip: "456", timestamp: "2021-01-01", domain: "sintaxi.com", status: 304, method: "get", device: "desktop", path: "/contact", referrer: null }
   , { ip: "456", timestamp: "2021-01-01", domain: "sintaxi.com", status: 304, method: "get", device: "desktop", path: "/contact", referrer: null}
-  , { ip: "123", timestamp: "2021-01-02", domain: "sintaxi.com", status: 200, method: "get", device: "desktop", path: "/blog/hi", referrer: "twitter.com/status/12345" }
-  , { ip: "123", timestamp: "2021-01-02", domain: "sintaxi.com", status: 304, method: "get", device: "desktop", path: "/blog/hi", referrer: "twitter.com/status/12345" }
-  , { ip: "456", timestamp: "2021-01-02", domain: "sintaxi.com", status: 304, method: "get", device: "desktop", path: "/blog/hi", referrer: "twitter.com/status/12345" }
-  , { ip: "456", timestamp: "2021-01-02", domain: "sintaxi.com", status: 304, method: "get", device: "desktop", path: "/blog/hi", referrer: "twitter.com/status/12345" }
-  , { ip: "123", timestamp: "2021-01-02", domain: "sintaxi.com", status: 404, method: "get", device: "tablet",  path: "/not-exist", referrer: "twitter.com/status/6789" }
-  , { ip: "123", timestamp: "2021-01-02", domain: "sintaxi.com", status: 404, method: "get", device: "tablet",  path: "/not-exist", referrer: "twitter.com/status/6789" }
+  , { ip: "123", timestamp: "2021-01-02", domain: "sintaxi.com", status: 200, method: "get", device: "desktop", path: "/blog/hi", referrer: "https://twitter.com/status/12345" }
+  , { ip: "123", timestamp: "2021-01-02", domain: "sintaxi.com", status: 304, method: "get", device: "desktop", path: "/blog/hi", referrer: "https://twitter.com/status/12345" }
+  , { ip: "456", timestamp: "2021-01-02", domain: "sintaxi.com", status: 304, method: "get", device: "desktop", path: "/blog/hi", referrer: "https://twitter.com/status/12345" }
+  , { ip: "456", timestamp: "2021-01-02", domain: "sintaxi.com", status: 304, method: "get", device: "desktop", path: "/blog/hi", referrer: "https://twitter.com/status/12345" }
+  , { ip: "123", timestamp: "2021-01-02", domain: "sintaxi.com", status: 404, method: "get", device: "tablet",  path: "/not-exist", referrer: "https://twitter.com/status/6789" }
+  , { ip: "123", timestamp: "2021-01-02", domain: "sintaxi.com", status: 404, method: "get", device: "tablet",  path: "/not-exist", referrer: "https://twitter.com/status/6789" }
 ]
